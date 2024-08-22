c1103a5a502c709cbb35285df5b830e936b85761
misaja <mauro.isaja@gmail.com>
2024-08-22 10:47:31 +0200

    final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,16 +5,6 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-// Function to check if the user exists
-/*
-const doesExist = (username) => {
-  let userswithsamename = users.filter((user) => {
-    return user.username === username;
-  });
-  return userswithsamename.length > 0;
-};
-*/
-
 public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -32,22 +22,41 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
+  // with async/await
+  let f = async function () {
+    const action = new Promise((resolve, reject) => {
+      resolve (res.status(200).send(JSON.stringify(books, null, ' ')));
+    });
+    await action;
+    console.log("Promise fullfilled!");
+  };
+  f();
+  /* regular style
   let txt = JSON.stringify(books, null, ' ');
   return res.status(200).send(txt);
+  */
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
+  // with Promise
+  const action = new Promise((resolve, reject) => {
+    let match = books[req.params.isbn];
+    if (match) {
+      resolve (res.status(200).send(JSON.stringify(match, null, ' ')));
+    } else {
+      reject (res.status(404).send());
+    }
+  });
+  action.then(() => {console.log("Promise fullfilled!")}).catch(console.error());
+  /* regular style
   let match = books[req.params.isbn];
-  /*
-  let booksArray = Object.values(books);
-  let match = booksArray.find((item) => item.isbn === req.params.isbn );
-  */
   if (match) {
     return res.status(200).send(JSON.stringify(match, null, ' '));
   } else {
     return res.status(404).send();
   }
+  */
 });
 
 //  Get book reviews
@@ -62,33 +71,50 @@ public_users.get('/review/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  /*
+  // with Promise
+  const action = new Promise((resolve, reject) => {
+    let booksArray = Object.values(books);
+    let matches = booksArray.filter((item) => item.author.indexOf(req.params.author) > -1);
+    if (matches.length > 0) {
+      resolve (res.status(200).send(JSON.stringify(matches, null, ' ')));
+    } else {
+      reject (res.status(404).send());
+    }
+  });
+  action.then(() => {console.log("Promise fullfilled!")}).catch(console.error());
+  /* regular style
   let booksArray = Object.values(books);
-  let match = booksArray.find((item) => item.author === req.params.author);
-  if (match) {
-    return res.status(200).send(JSON.stringify(match, null, ' '));
+  let matches = booksArray.filter((item) => item.author.indexOf(req.params.author) > -1);
+  if (matches.length > 0) {
+    return res.status(200).send(JSON.stringify(matches, null, ' '));
   } else {
     return res.status(404).send();
   }
   */
-  let booksArray = Object.values(books);
-  let matches = booksArray.filter((item) => item.author.indexOf(req.params.author) > -1  );
-  if (matches.length > 0) {
-    return res.status(200).send(JSON.stringify(matches, null, ' '));
-  } else {
-    return res.status(404).send();
-  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
+  // with Promise
+  const action = new Promise((resolve, reject) => {
+    let booksArray = Object.values(books);
+    let matches = booksArray.filter((item) => item.title.indexOf(req.params.title) > -1);
+    if (matches.length > 0) {
+      resolve (res.status(200).send(JSON.stringify(matches, null, ' ')));
+    } else {
+      reject (res.status(404).send());
+    }
+  });
+  action.then(() => {console.log("Promise fullfilled!")}).catch(console.error());
+  /* regular style
   let booksArray = Object.values(books);
-  let matches = booksArray.filter((item) => item.title.indexOf(req.params.title) > -1  );
+  let matches = booksArray.filter((item) => item.title.indexOf(req.params.title) > -1);
   if (matches.length > 0) {
     return res.status(200).send(JSON.stringify(matches, null, ' '));
   } else {
     return res.status(404).send();
   }
+  */
 });
 
 module.exports.general = public_users;
